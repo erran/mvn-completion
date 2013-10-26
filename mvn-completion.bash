5666fa29faec73a39e8b40fd08def5fb84db79f8
Erran Carey <me@errancarey.com>
2013-10-26 02:51:13 -0500

    Sort subcommands in ASC alphabetical order

--- mvn-completion.bash
+++ mvn-completion.bash
@@ -28,7 +28,7 @@
 
 _mvn_completion() {
   local current=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "clean compile install cargo:run exec:java" -- $current) )
+  COMPREPLY=( $(compgen -W "clean compile cargo:run exec:java install" -- $current) )
 }
 
 complete -F _mvn_completion mvn
